6e1415bf612d2ce6bd89602b408d90b54dce7e88
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-06-30 14:44:14 +0200

    Add: Route API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Non ci serve
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('/test', function () {
+//     $user = [
+//         'name' => 'pippo',
+//         'role' => 'student'
+//     ];
+//         return response()->json($user);
+//     });
+
+Route::get('/projects', [ProjectController::class, 'index']);
